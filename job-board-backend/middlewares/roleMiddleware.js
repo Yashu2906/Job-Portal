92e045bf9555a2f8078b0d802997c41a58da53cd
Yash Patel <patelyashu001@gmail.com>
2025-08-23 13:06:39 +0530

    roleMiddleware

---
+++ job-board-backend/middlewares/roleMiddleware.js
@@ -0,0 +1,10 @@
+
+const roleMiddleWare = (roles) =>{
+    return (req,res,next) =>{
+        if(!roles.includes(req.user.role)){
+            return res.status(403).json({success:false , message:"Access denied"})
+        }
+        next();
+    }
+}
+module.exports = roleMiddleWare;
--- job-board-backend/routes/jobRoutes.js
+++ job-board-backend/routes/jobRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/authMiddleware');
 const {createJob,getJob, getJobById} = require('../controllers/jobController');
+const roleMiddleWare = require('../middlewares/roleMiddleware');
 
 const jobRouter = express.Router();
 
-jobRouter.post('/createJob',authMiddleware ,createJob);
+jobRouter.post('/createJob',authMiddleware,roleMiddleWare(["Company"]) ,createJob);
 jobRouter.get('/',getJob);
 jobRouter.get('/:id',getJobById)
 
