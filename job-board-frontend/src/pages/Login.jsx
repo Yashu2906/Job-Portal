a5299fd44881ecd74bb7ab93a694c473e444a6fe
Yash Patel <patelyashu001@gmail.com>
2025-08-31 19:56:37 +0530

    Login Register

--- job-board-frontend/src/pages/Login.jsx
+++ job-board-frontend/src/pages/Login.jsx
@@ -1,48 +1,90 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faUser } from "@fortawesome/free-regular-svg-icons";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
 
 const Login = () => {
   return (
-    <div className="main">
-      <div className="login">
-        <form>
-          <p>Login</p>
-          <div>
-            <input type="email" placeholder="Email" required></input>
-            <input type="password" placeholder="Password" required></input>
-          </div>
-          <div>
-            <button>Log In</button>
-          </div>
-        </form>
-      </div>
-      <div>
-        <form>
-          <p>Register</p>
-          <div>
-            <input type="text" placeholder="Name" required></input>
-            <input type="email" placeholder="Email" required></input>
-            <input type="password" placeholder="Password" required></input>
-            <div>
-              <label htmlFor="roles">Role </label>
-              <select name="role">
-                <option value="Jobseekers">Jobseekers</option>
-                <option value="Company">Company</option>
-                <option value="Admin">Admin</option>
-              </select>
+    <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-[#e2e2e2] to-[#c9d6ff]">
+      <div className="w-[1100px] h-[700px] bg-[#fff] overflow-hidden rounded-3xl shadow-2xl flex">
+        <div className="w-1/2 h-full flex flex-col justify-center items-center bg-white text-[#333]">
+          <form className="w-[450px] flex flex-col ">
+            <h1 className="text-5xl font-bold text-center">Login</h1>
+
+            <div className="relative flex mt-10">
+              <input
+                className="w-full h-15 px-5 pr-12 bg-[#eee] text-[20px] text-[#333] font-medium rounded-xl outline-none"
+                type="email"
+                placeholder="Email"
+                required
+              />
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                className="text-gray-500 absolute top-1/2 right-5 transform -translate-y-1/2 text-2xl"
+              />
             </div>
-          </div>
-        </form>
-      </div>
-      <div>
-        <div>
-          <h1>Hello, Welcome!</h1>
-          <p>Don't have an account?</p>
-          <button>Register</button>
+
+            <div className="relative flex mt-8">
+              <input
+                className="w-full h-15 px-5 pr-14 bg-[#eee] text-[20px] text-[#333] font-medium rounded-xl outline-none"
+                type="password"
+                placeholder="Password"
+                required
+              />
+              <FontAwesomeIcon
+                icon={faLock}
+                className="text-gray-500 absolute top-1/2 right-5 transform -translate-y-1/2 text-2xl"
+              />
+            </div>
+
+            <button className="w-full h-14 bg-[#7494ec] rounded-[8px] text-[#fff] text-[22px] font-bold cursor-pointer mt-8">
+              Login
+            </button>
+          </form>
         </div>
-        <div>
-          <h1>Welcome Back!</h1>
-          <p>Already have an account?</p>
-          <button>Login</button>
+        <div className="w-1/2 h-full flex flex-col justify-center items-center bg-white text-[#333]">
+          <form className="w-[450px] flex flex-col">
+            <h1 className="text-5xl font-bold text-center">Register</h1>
+            <div className="relative flex mt-10">
+              <input
+                className="w-full h-15 px-5 pr-12 bg-[#eee] text-[20px] text-[#333] font-medium rounded-xl outline-none"
+                type="name"
+                placeholder="Name"
+                required
+              />
+              <FontAwesomeIcon
+                icon={faUser}
+                className="text-gray-500 absolute top-1/2 right-5 transform -translate-y-1/2 text-2xl"
+              />
+            </div>
+            <div className="relative flex mt-7">
+              <input
+                className="w-full h-15 px-5 pr-12 bg-[#eee] text-[20px] text-[#333] font-medium rounded-xl outline-none"
+                type="email"
+                placeholder="Email"
+                required
+              />
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                className="text-gray-500 absolute top-1/2 right-5 transform -translate-y-1/2 text-2xl"
+              />
+            </div>
+            <div className="relative flex mt-7">
+              <input
+                className="w-full h-15 px-5 pr-14 bg-[#eee] text-[20px] text-[#333] font-medium rounded-xl outline-none"
+                type="password"
+                placeholder="Password"
+                required
+              />
+              <FontAwesomeIcon
+                icon={faLock}
+                className="text-gray-500 absolute top-1/2 right-5 transform -translate-y-1/2 text-2xl"
+              />
+            </div>
+            <button className="w-full h-14 bg-[#7494ec] rounded-[8px] text-[#fff] text-[22px] font-bold cursor-pointer mt-7">
+              Register
+            </button>
+          </form>
         </div>
       </div>
     </div>
