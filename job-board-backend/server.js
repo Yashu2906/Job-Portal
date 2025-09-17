307b17e0c7dc75b0ec56dbd64cc9049dd8da7e4a
Yash Patel <patelyashu001@gmail.com>
2025-09-17 10:08:43 +0530

    server.js

--- job-board-backend/server.js
+++ job-board-backend/server.js
@@ -12,14 +12,22 @@ const PORT = process.env.PORT || 3000;
 
 connectDB();
 
-app.use(express.json());
 app.use(
   cors({
     origin: "https://job-x.netlify.app",
-    methods: "GET,POST,PUT,DELETE",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://job-x.netlify.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use(express.json());
 
 app.get("/api", (req, res) => {
   res.send("Api working");
